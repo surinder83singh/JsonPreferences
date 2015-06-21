397382910ac573727777857f1ea5d614cbd06b4b
surinder singh <surinder83singh@gmail.com>
2015-06-21 12:15:16 +0530

    Initial Commit

---
+++ JsonPreferences/JsonPreferences.h
@@ -0,0 +1,15 @@
+//
+//  JsonPreferences.h
+//  JsonPreferences
+//
+//  Created by Surinder Singh on 21/06/15.
+//  Copyright (c) 2015 Surinder Singh. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface JsonPreferences : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ JsonPreferences/JsonPreferences.m
@@ -0,0 +1,17 @@
+//
+//  JsonPreferences.m
+//  JsonPreferences
+//
+//  Created by Surinder Singh on 21/06/15.
+//  Copyright (c) 2015 Surinder Singh. All rights reserved.
+//
+
+#import "JsonPreferences.h"
+
+@implementation JsonPreferences
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
